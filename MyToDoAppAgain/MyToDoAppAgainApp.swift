5f34cf7bc9116b015730e894009c17c4a16431fe
ipeerless <ipeerless0@gmail.com>
2024-12-25 13:21:57 +0200

    Initial Commit

---
+++ MyToDoAppAgain/MyToDoAppAgainApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDoAppAgainApp.swift
+//  MyToDoAppAgain
+//
+//  Created by ipeerless on 25/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyToDoAppAgainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
